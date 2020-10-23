76278e9cbfcf05298e7f689503e7bb1d8bf798b8
newJorney <alabdullah.akash99@gmail.com>
2020-10-23 20:59:04 +0600

    NA

--- uri/try.cpp
+++ uri/try.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <algorithm> 
 using namespace std;
 
 int main(){
-    int x;
-    cin>>x;
+    int x,y;
+    char am[20];
+    scanf("%d: %d %s",&x,&y,&am);
     cout<<x<<endl;
+    cout<<y<<endl;
+    cout<<am<<endl;
 }
