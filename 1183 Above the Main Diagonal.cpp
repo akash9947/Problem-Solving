1e58eb056065ddded7fc6e6e960d37a8c7c22778
Al Abdullah Akash <alabdullah.akash99@gmail.com>
2021-05-25 12:13:53 +0600

    aa

--- 1183 Above the Main Diagonal.cpp
+++ 1183 Above the Main Diagonal.cpp
@@ -6,33 +6,34 @@ using namespace std;
 int main()
 {
     double m[12][12],sum=0.0;
-    int a;
     char n;
-    cin>>a>>n;
+    int z=1;
+    cin>>n;
 
     for(int i=0;i<12;i++)
     {
-        for (int j=0;j<12;j++) 
+        for(int j=0;j<12;j++)
         {
             cin>>m[i][j];
         }
-        
-     }
+    }
 
-     for (int i=0;i<12;i++)
-     {
-         sum += m[i][a];
-     }
+    for (int i=0;i<12;i++)
+    {
+        for(int j=z;j<12;j++)
+        {
+            sum += m[i][j];
+        }
+        z++;
+    }
 
     if(n =='S')
     {
         cout<<setprecision(1)<<fixed<<sum<<endl;
     }
-
-    else
+    else if (n== 'M')
     {
-        cout<<setprecision(1)<<fixed<<sum/12<<endl;
+        cout<<setprecision(1)<<fixed<<sum/66.0<<endl;
     }
-
-
+    return 0;
 }
