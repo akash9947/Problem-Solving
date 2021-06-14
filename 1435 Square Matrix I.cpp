2a5f283bc802522e3921f2839beedb59bc22534d
Al Abdullah Akash <alabdullah.akash99@gmail.com>
2021-06-14 15:46:01 +0600

    aa

--- 1435 Square Matrix I.cpp
+++ 1435 Square Matrix I.cpp
@@ -7,14 +7,11 @@ int main()
 {
     int a;
     while (cin>>a && a!=0)
-    {     
-    
+    {        
         for(int i=0;i<a;i++)
         {
             for (int j=0;j<a;j++)
-            {
-                
-
+            {               
                 int top=j+1;
                 int left=i+1;
                 int bottom=a-i;
@@ -22,13 +19,13 @@ int main()
                 cout<<setw(3)<<min(min(top,bottom),min(left,right));
                 if(j!=a-1){
                     cout<<" ";
-                }
-                
+                }                
             }
-        cout<<" "<<endl;
+            cout<<endl;
         }
+        cout<<endl;
     }
-    
+   
 }
     
     
